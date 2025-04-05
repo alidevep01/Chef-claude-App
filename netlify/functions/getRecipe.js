8f198555997bee5e5fb4d68a211f738a785e94a8
Mohammad <mohammadali0326@gmail.com>
2025-04-05 13:53:22 +0530

    updated api key import

--- netlify/functions/getRecipe.js
+++ netlify/functions/getRecipe.js
@@ -10,7 +10,7 @@ export const handler = async (event) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "x-api-key": process.env.ANTHROPIC_API_KEY,
+        "x-api-key": import.meta.env.VITE_ANTHROPIC_API_KEY,
         "anthropic-version": "2023-06-01",
       },
       body: JSON.stringify({
